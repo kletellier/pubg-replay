9506b5529e54ee68f7c2c05a92444a816c0a0299
kletellier <gregory.letellier@gmail.com>
2018-08-13 14:29:47 +0200

    Enable telemetry gzip request

--- app/Classes/Request.php
+++ app/Classes/Request.php
@@ -12,12 +12,19 @@ class Request{
 		try 
 		{
 			$ch = curl_init();
+
+			$headers = array(
+		        'Accept-Encoding: gzip'
+		    );      
+
 		    curl_setopt($ch, CURLOPT_URL, $url);
 		    curl_setopt($ch, CURLOPT_HEADER, 0);
+		    curl_setopt($ch, CURLOPT_ENCODING, "gzip");
+		    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 		    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 			curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-		 	curl_setopt($ch, CURLOPT_TIMEOUT, 45);
+		 	curl_setopt($ch, CURLOPT_TIMEOUT, 60);
 		    $data = curl_exec($ch);
 		    curl_close($ch);
 		} 
