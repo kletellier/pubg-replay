e843f3af7dce9475fb24945dae9825285d95c1fb
kletellier <gregory.letellier@gmail.com>
2018-08-13 14:49:53 +0200

    Missing header from telemetry request

--- app/Classes/Request.php
+++ app/Classes/Request.php
@@ -12,26 +12,27 @@ class Request{
 		try 
 		{
 			$ch = curl_init();
-
-			$headers = array(
-		        'Accept-Encoding: gzip'
-		    );      
-
+				 
 		    curl_setopt($ch, CURLOPT_URL, $url);
-		    curl_setopt($ch, CURLOPT_HEADER, 0);
-		    curl_setopt($ch, CURLOPT_ENCODING, "gzip");
-		    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		    curl_setopt($ch, CURLOPT_HEADER, 0);		     
 		    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-			curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-		 	curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+			curl_setopt($ch, CURLINFO_HEADER_OUT, true);  
+			curl_setopt($ch, CURLOPT_ENCODING, "gzip");
+		 	curl_setopt($ch, CURLOPT_TIMEOUT, 45);   
+		 	$headers = [ 
+			    'Accept: application/vnd.api+json',
+			    'Accept-Encoding: gzip',
+			];
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 		    $data = curl_exec($ch);
 		    curl_close($ch);
 		} 
 		catch (Exception $e) 
 		{
-		
+			
 		}
-	    return $data;
+	    return $data; 
+
 	}
 }
